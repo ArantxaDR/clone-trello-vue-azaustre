5a919ad39bb962709d7750187f79ce77f5944a64
ArantxaDR <arantxaderuiz@gmail.com>
2022-10-27 09:25:34 +0200

    header and  footer layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h3>Mis paneles</h3>
+    <div class="boards-collection">
+      <input
+        type="text"
+        placeholder="Añade un nuevo panel"
+        v-model="boardName"
+        @keyup.enter="add()"
+      />
+      <board-card
+        v-for="(board, index) in boards"
+        :key="index"
+        :name="board.name"
+        :id="board.id"
+      >
+      </board-card>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "home-view",
+};
+</script>
+<style lang="scss" scoped>
+h3 {
+  text-align: left;
+  margin: 1.5rem;
+}
+</style>
